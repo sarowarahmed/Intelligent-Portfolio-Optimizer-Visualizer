bc864a66045d7f836c6fdc0fa712c81687e0b458
Sarowar Ahmed <sectumsempra.cruciobliviate@gmail.com>
2025-05-02 22:55:49 +0530

    Updated utils.py file

--- utils.py
+++ utils.py
@@ -1,3 +1,18 @@
 import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
+
+def plot_portfolio_weights(weights):
+    labels = list(weights.keys())
+    sizes = list(weights.values())
+
+    fig, ax = plt.subplots()
+    ax.pie(sizes, labels = labels, autopct='%1.1f%%', startangle=140)
+    ax.axis('equal')
+    return fig
+
+def calculate_cumulative_returns(price_data, weights):
+    returns = price_data.pct_change().dropna()
+    portfolio_returns = returns.dot(pd.series(weights))
+    cumulative_returns = (1+portfolio_returns).cumprod()
+    return cumulative_returns
