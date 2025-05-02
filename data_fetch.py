2d0121e043b58ec39fce92a6a85b2cb1a573bbe7
Sarowar Ahmed <sectumsempra.cruciobliviate@gmail.com>
2025-05-02 21:49:04 +0530

    Updated data_fetch.py

--- data_fetch.py
+++ data_fetch.py
@@ -5,6 +5,6 @@ def fetch_price_data(tickers, start_date, end_date):
     data = yf.download(tickers, start=start_date, end=end_date, auto_adjust=True)
     
     if isinstance(tickers, str):
-        return data[['Adj Close']]  # always return a DataFrame
+        return data[['Close']]  # always return a DataFrame
     else:
-        return data['Adj Close']
+        return data['Close']
